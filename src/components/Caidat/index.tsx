707528bb380fc861c61b8c0a279d2021184578bb
Ductrann <ductran27082000@gmail.com>
2022-06-01 18:51:02 +0700

    Update4

--- src/components/Caidat/index.tsx
+++ src/components/Caidat/index.tsx
@@ -1,5 +1,5 @@
-import React , {useEffect } from "react";
-import { Layout, Table, Typography } from "antd";
+import React , {useEffect, useState } from "react";
+import { Layout, Table, Typography, Checkbox, Col, DatePicker,Modal,Row, Select, TimePicker } from "antd";
 import Button from "../../assets/imgs/icons/Button";
 import Search from "../../assets/imgs/icons/Search";
 import {EditorIcon, FilterIcon} from "../../assets/imgs/icons/navBarIcon";
@@ -8,216 +8,412 @@ import {dataTicketPage} from "../Store/data"
 
 
 interface Props {
-    setTagIndex: React.Dispatch<React.SetStateAction<string>>;
-  }
-  
-  const Caidat = ({ setTagIndex }: Props) => {
-    useEffect(() => {
-      setTagIndex("Caidat");
-    });
-    const columns = [
-      {
-        title: "STT",
-        dataIndex: "stt",
-        render: (stt: string) => {
-          return <div>{stt}</div>;
-        },
+  setTagIndex: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Caidat = ({ setTagIndex }: Props) => {
+  useEffect(() => {
+    setTagIndex("Caidat");
+  });
+
+  const [modal, setModal] = useState(false);
+
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "stt",
+      render: (stt: string) => {
+        return <div>{stt}</div>;
       },
-      {
-        title: () => <div style={{}}>Mã gói</div>,
-        dataIndex: "code",
-        render: (stt: string) => {
-          return <div style={{}}>{stt}</div>;
-        },
+    },
+    {
+      title: () => <div style={{}}>Mã gói</div>,
+      dataIndex: "code",
+      render: (stt: string) => {
+        return <div style={{}}>{stt}</div>;
       },
-      {
-        title: () => <div style={{}}>Tên gói</div>,
-        dataIndex: "code",
-        render: (stt: string) => {
-          return <div style={{}}>{stt}</div>;
-        },
+    },
+    {
+      title: () => <div style={{}}>Tên gói</div>,
+      dataIndex: "code",
+      render: (stt: string) => {
+        return <div style={{}}>{stt}</div>;
       },
-      {
-        title: () => (
-          <div
-            style={{
-              textAlign: "right",
-            }}
-          >
-            Ngày áp dụng
-          </div>
-        ),
-        dataIndex: "dateUsed",
-        render: (date: string) => {
-          return (
-            <div
-              style={{
-                textAlign: "right",
-              }}
-            >
-              {date}
-            </div>
-          );
-        },
-      },
-      {
-        title: () => (
-          <div
-            style={{
-              textAlign: "right",
-            }}
-          >
-            Ngày hết hạn
-          </div>
-        ),
-        dataIndex: "dateExport",
-        render: (date: string) => {
-          return (
-            <div
-              style={{
-                textAlign: "right",
-              }}
-            >
-              {date}
-            </div>
-          );
-        },
-      },
-      {
-        title: () => <div style={{}}>Giá vé {"(VND/Combo)"}</div>,
-        dataIndex: "code",
-        render: (stt: string) => {
-          return <div style={{}}>{stt}</div>;
-        },
-      },
-      {
-        title: () => <div style={{}}>Tình trạng</div>,
-        dataIndex: "status",
-        render: (status: number) => {
-          switch (status) {
-            case 1:
-              return (
-                <div
-                  style={{
-                    width: "fit-content",
-                    padding: "0 12px",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "12px",
-                    fontWeight: "500",
-                    color: "#03AC00",
-                    backgroundColor: "#DEF7E0",
-                    border: "1px solid #03AC00",
-                    borderRadius: "4px",
-                    height: "31px",
-                  }}
-                >
-                  <span style={{ fontSize: "18px", marginRight: "8px" }}>●</span>
-                  Đang áp dụng
-                </div>
-              );
-            default:
-              return (
-                <div
-                  style={{
-                    width: "fit-content",
-                    padding: "0 12px",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "12px",
-                    fontWeight: "500",
-                    color: "#FD5959",
-                    backgroundColor: "#F8EBE8",
-                    border: "1px solid #FD5959",
-                    borderRadius: "4px",
-                    height: "31px",
-                  }}
-                >
-                  <span style={{ fontSize: "18px", marginRight: "8px" }}>●</span>
-                  Tắt
-                </div>
-              );
-          }
-        },
-      },
-  
-      {
-        title: "",
-        render: () => {
-          return (
-            <div
-              style={{
-                color: "#FF993C",
-                fontWeight: "500",
-                fontSize: "14px",
-                display: "flex",
-                alignItems: "center",
-                cursor: "pointer",
-              }}
-            >
-              <EditorIcon />
-              <span style={{ marginLeft: "4px" }}> Cập nhật</span>
-            </div>
-          );
-        },
-      },
-    ];
-  
-    return (
-      <Layout.Content
-        style={{
-          borderRadius: "24px",
-          minHeight: "963px",
-          backgroundColor: "white",
-          padding: "34px 24.5px",
-        }}
-      >
-        <Typography.Title
-          style={{ marginBottom: "46px", fontSize: "36px", fontWeight: "700" }}
-        >
-          Danh sách gói vé
-        </Typography.Title>
+    },
+    {
+      title: () => (
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
+            textAlign: "right",
           }}
         >
-          <Search size="445px" placeholder="Tìm bằng số vé" />
-          <div style={{ marginTop: "-4px" }}>
-            <Button margin="0 10px" width="128px">
-              <span
+          Ngày áp dụng
+        </div>
+      ),
+      dataIndex: "dateUsed",
+      render: (date: string) => {
+        return (
+          <div
+            style={{
+              textAlign: "right",
+            }}
+          >
+            {date}
+          </div>
+        );
+      },
+    },
+    {
+      title: () => (
+        <div
+          style={{
+            textAlign: "right",
+          }}
+        >
+          Ngày hết hạn
+        </div>
+      ),
+      dataIndex: "dateExport",
+      render: (date: string) => {
+        return (
+          <div
+            style={{
+              textAlign: "right",
+            }}
+          >
+            {date}
+          </div>
+        );
+      },
+    },
+    {
+      title: () => <div style={{}}>Giá vé {"(VND/Vé)"}</div>,
+      dataIndex: "code",
+      render: () => {
+        return <div style={{}}>90.000 VNĐ</div>;
+      },
+    },
+    {
+      title: () => <div style={{}}>Giá vé {"(VND/Combo)"}</div>,
+      dataIndex: "code",
+      render: () => {
+        return <div style={{}}>360.000 VNĐ/4 Vé</div>;
+      },
+    },
+    {
+      title: () => <div style={{}}>Tình trạng</div>,
+      dataIndex: "status",
+      render: (status: number) => {
+        switch (status) {
+          case 1:
+            return (
+              <div
                 style={{
-                  display: "inline-block",
-                  transform: "translate(-6px, 4px)",
+                  width: "fit-content",
+                  padding: "0 12px",
+                  display: "flex",
+                  alignItems: "center",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  color: "#03AC00",
+                  backgroundColor: "#DEF7E0",
+                  border: "1px solid #03AC00",
+                  borderRadius: "4px",
+                  height: "31px",
                 }}
               >
-                <FilterIcon />
-              </span>
-              Lọc vé
-            </Button>
-            <Button width="180px">Xuất file (.csv)</Button>
-          </div>
-        </div>
-        <Table
-          bordered={false}
-          style={{ marginTop: "30px", alignItems: "center" }}
-          dataSource={dataTicketPage}
-          columns={columns}
-          pagination={{
-            pageSize: 12,
-            style: {
-              marginTop: "48px",
+                <span style={{ fontSize: "18px", marginRight: "8px" }}>●</span>
+                Đang áp dụng
+              </div>
+            );
+          default:
+            return (
+              <div
+                style={{
+                  width: "fit-content",
+                  padding: "0 12px",
+                  display: "flex",
+                  alignItems: "center",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  color: "#FD5959",
+                  backgroundColor: "#F8EBE8",
+                  border: "1px solid #FD5959",
+                  borderRadius: "4px",
+                  height: "31px",
+                }}
+              >
+                <span style={{ fontSize: "18px", marginRight: "8px" }}>●</span>
+                Tắt
+              </div>
+            );
+        }
+      },
+    },
+
+    {
+      title: "",
+      render: () => {
+        return (
+          <div
+            style={{
+              color: "#FF993C",
+              fontWeight: "500",
+              fontSize: "14px",
               display: "flex",
-              justifyContent: "center",
-            },
-            size: "small",
-            prevIcon: () => <span style={{ color: "#A5A8B1" }}> &#9664;</span>,
-            nextIcon: <span style={{ color: "#A5A8B1" }}> &#9654;</span>,
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+          >
+            <EditorIcon />
+            <span style={{ marginLeft: "4px" }}> Cập nhật</span>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Layout.Content
+      style={{
+        borderRadius: "24px",
+        minHeight: "963px",
+        backgroundColor: "white",
+        padding: "34px 24.5px",
+      }}
+    >
+      <Typography.Title
+        style={{ marginBottom: "46px", fontSize: "36px", fontWeight: "700" }}
+      >
+        Danh sách gói vé
+      </Typography.Title>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Search size="445px" placeholder="Tìm bằng số vé" />
+        <div style={{ marginTop: "-4px" }}>
+          <Button padding="0 24px" margin="0 10px" width="fit-content">
+            <span
+              style={{
+                display: "inline-block",
+                transform: "translate(-6px, 4px)",
+              }}
+            ></span>
+            Xuất file (.csv)
+          </Button>
+          <Button width="180px" type="primary" onClick={() => setModal(true)}>
+            Thêm gói vé
+          </Button>
+        </div>
+      </div>
+      <Table
+        bordered={false}
+        style={{ marginTop: "30px", alignItems: "center" }}
+        dataSource={dataTicketPage}
+        columns={columns}
+        pagination={{
+          pageSize: 12,
+          style: {
+            marginTop: "48px",
+            display: "flex",
+            justifyContent: "center",
+          },
+          size: "small",
+          prevIcon: () => <span style={{ color: "#A5A8B1" }}> &#9664;</span>,
+          nextIcon: <span style={{ color: "#A5A8B1" }}> &#9654;</span>,
+        }}
+      />
+      <Modal
+        visible={modal}
+        onOk={() => setModal(false)}
+        closeIcon={<></>}
+        width="750px"
+        bodyStyle={{ borderRadius: "16px" }}
+        onCancel={() => setModal(false)}
+        cancelText="Hủy"
+        okButtonProps={{
+          style: {
+            marginRight: "230px",
+            height: "40px",
+            padding: "0 48px",
+            borderRadius: "8px",
+            backgroundColor: "#FF993C",
+            border: "2px solid #FF993C",
+            fontSize: "16px",
+            fontWeight: "600",
+          },
+        }}
+        cancelButtonProps={{
+          style: {
+            height: "40px",
+            padding: "0 48px",
+            borderRadius: "8px",
+            border: "2px solid #FF993C",
+            fontSize: "16px",
+            color: "#FF993C",
+            fontWeight: "600",
+          },
+        }}
+        okText="Lưu"
+      >
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <span
+            style={{ fontSize: "24px", fontWeight: "600", marginTop: "-4px" }}
+          >
+            Thêm gói vé
+          </span>
+        </div>
+        <div style={{ marginTop: "24px" }}>
+          <span style={{ fontSize: "16px", margin: "opx 4px" }}>
+            Tên gói vé
+          </span>
+          <span style={{ color: "red" }}> *</span>
+        </div>
+        <input
+          type="text"
+          placeholder="Nhập tên gói vé"
+          style={{
+            padding: "20px",
+            marginTop: "4px",
+            border: "1px solid #A5A8B1",
+            borderRadius: "8px",
+            height: "40px",
+            width: "370px",
           }}
         />
-      </Layout.Content>
-    );
-  };
-  
-  export default Caidat;
+        <Row>
+          <Col span={11}>
+            <div style={{ marginTop: "24px", marginBottom: "4px" }}>
+              <span style={{ fontSize: "16px", margin: "opx 4px" }}>
+                Ngày áp dụng
+              </span>
+            </div>
+            <DatePicker
+              style={{ height: "40px", width: "145px" }}
+              placeholder="dd/mm/yy"
+            />
+            <TimePicker
+              use12Hours
+              format="hh:mm:ss"
+              placeholder="hh:mm:ss"
+              style={{ width: 140, height: "40px", marginLeft: "8px" }}
+            />
+          </Col>
+          <Col span={12}>
+            <div style={{ marginTop: "24px", marginBottom: "4px" }}>
+              <span style={{ fontSize: "16px", margin: "0px 4px" }}>
+                Ngày hết hạn
+              </span>
+            </div>
+            <DatePicker
+              style={{ height: "40px", width: "145px" }}
+              placeholder="dd/mm/yy"
+            />
+            <TimePicker
+              placeholder="hh/mm/yy"
+              use12Hours
+              format="h:mm:ss"
+              style={{ width: 140, height: "40px", marginLeft: "8px" }}
+            />
+          </Col>
+        </Row>
+        <div style={{ fontSize: "16px", margin: "28px 0 0 4px" }}>
+          Giá vé áp dụng
+        </div>
+        <div
+          style={{ display: "flex", alignItems: "center", fontSize: "16px" }}
+        >
+          <Checkbox style={{ width: "20px" }}></Checkbox>Vé lẻ (vnđ/vé) với giá
+          <div
+            style={{
+              color: "#A5A8B1",
+              alignItems: "center",
+              margin: "0 4px",
+              padding: "0 8px",
+              display: "inline-flex",
+              height: "40px",
+              width: "150px",
+              background: "#F1F4F8",
+              borderRadius: "8px",
+            }}
+          >
+            Giá vé
+          </div>
+          /vé
+        </div>
+        <div
+          style={{
+            marginTop: "8px",
+            display: "flex",
+            alignItems: "center",
+            fontSize: "16px",
+          }}
+        >
+          <Checkbox style={{ width: "20px" }}></Checkbox>Combo vé với giá
+          <div
+            style={{
+              color: "#A5A8B1",
+              alignItems: "center",
+              margin: "0 4px",
+              padding: "0 8px",
+              display: "inline-flex",
+              height: "40px",
+              width: "150px",
+              background: "#F1F4F8",
+              borderRadius: "8px",
+            }}
+          >
+            Giá vé
+          </div>
+          /
+          <div
+            style={{
+              color: "#A5A8B1",
+              alignItems: "center",
+              margin: "0 4px",
+              padding: "0 8px",
+              display: "inline-flex",
+              height: "40px",
+              width: "150px",
+              background: "#F1F4F8",
+              borderRadius: "8px",
+            }}
+          >
+            Giá vé
+          </div>
+          vé
+        </div>
+        <div style={{ fontSize: "16px", margin: "28px 0 0 4px" }}>
+          Tình trạng
+        </div>
+        <Select
+          showSearch
+          size="large"
+          style={{ width: "180px", marginBottom: "20px" }}
+          placeholder="Đang áp dụng"
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            (option!.children as unknown as string).includes(input)
+          }
+          filterSort={(optionA, optionB) =>
+            (optionA!.children as unknown as string)
+              .toLowerCase()
+              .localeCompare(
+                (optionB!.children as unknown as string).toLowerCase()
+              )
+          }
+        ></Select>
+
+        <div>
+          <span style={{ color: "red" }}> *</span>{" "}
+          <span style={{ fontStyle: "italic" }}>là thông tin bắt buộc</span>
+        </div>
+      </Modal>
+    </Layout.Content>
+  );
+};
+
+export default Caidat;
--- src/components/Doisoatve/index.tsx
+++ src/components/Doisoatve/index.tsx
@@ -1,173 +1,232 @@
-import React, { useEffect } from "react";
-import { Layout, Table, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { Layout, Table, Typography, Col, DatePicker, Radio, RadioChangeEvent, Row  } from "antd";
 import { dataTicketPage } from "../Store/data";
 import Button from "../../assets/imgs/icons/Button";
 import Search from "../../assets/imgs/icons/Search";
 import { FilterIcon } from "../../assets/imgs/icons/navBarIcon";
-
+import moment from "moment"
 
 
 interface Props {
-    setTagIndex: React.Dispatch<React.SetStateAction<string>>;
-  }
-  
-  const Doisoatve = ({ setTagIndex }: Props) => {
-    useEffect(() => {
-      setTagIndex("Doisoatve");
-    });
-    const columns = [
-      {
-        title: "STT",
-        dataIndex: "stt",
-        render: (stt: string) => {
-          return <div>{stt}</div>;
-        },
+  setTagIndex: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Doisoatve = ({ setTagIndex }: Props) => {
+  useEffect(() => {
+    setTagIndex("Doisoatve");
+  });
+
+  const [value, setValue] = useState(1);
+
+  const onChange = (e: RadioChangeEvent) => {
+    setValue(e.target.value);
+  };
+
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "stt",
+      render: (stt: string) => {
+        return <div>{stt}</div>;
       },
-      {
-        title: () => <div style={{}}>Số vé</div>,
-        dataIndex: "code",
-        render: (stt: string) => {
-          return <div style={{}}>{stt}</div>;
-        },
+    },
+    {
+      title: () => <div style={{}}>Số vé</div>,
+      dataIndex: "code",
+      render: (stt: string) => {
+        return <div style={{}}>{stt}</div>;
       },
-      {
-        title: () => <div style={{}}>Tên sự kiện</div>,
-        render: (stt: string) => {
-          return <div style={{}}>Hội chợ triển lãm tiêu dùng 2021</div>;
-        },
+    },
+    {
+      title: () => <div style={{}}>Tên sự kiện</div>,
+      render: (stt: string) => {
+        return <div style={{}}>Hội chợ triển lãm tiêu dùng 2021</div>;
       },
-      {
-        title: () => (
+    },
+    {
+      title: () => (
+        <div
+          style={{
+            textAlign: "right",
+          }}
+        >
+          Ngày sử dụng
+        </div>
+      ),
+      dataIndex: "dateUsed",
+      render: (date: string) => {
+        return (
           <div
             style={{
               textAlign: "right",
             }}
           >
-            Ngày sử dụng
+            {date}
           </div>
-        ),
-        dataIndex: "dateUsed",
-        render: (date: string) => {
-          return (
-            <div
-              style={{
-                textAlign: "right",
-              }}
-            >
-              {date}
-            </div>
-          );
-        },
+        );
       },
-  
-      {
-        title: "Loại vé",
-        render: () => {
-          return <div>Vé cổng</div>;
-        },
+    },
+
+    {
+      title: "Loại vé",
+      render: () => {
+        return <div>Vé cổng</div>;
       },
-      {
-        title: "Cổng check-in",
-        render: () => {
-          return <div>Cổng 1</div>;
-        },
+    },
+    {
+      title: "Cổng check-in",
+      render: () => {
+        return <div>Cổng 1</div>;
       },
-      {
-        title: "",
-        render: () => {
-          return (
-            <div
-              style={{
-                color: "#A5A8B1",
-                fontWeight: "500",
-              }}
-            >
-              Chưa đối soát
-            </div>
-          );
-        },
-      },
-    ];
-  
-    return (
-      <div style={{ display: "flex" }}>
-        <Layout.Content
-          style={{
-            borderRadius: "24px",
-            maxWidth: "1097px",
-            minHeight: "963px",
-            backgroundColor: "white",
-            padding: "28px",
-            marginRight: "32px",
-          }}
-        >
-          <Typography.Title
-            style={{ marginBottom: "46px", fontSize: "36px", fontWeight: "700" }}
-          >
-            Đối soát vé
-          </Typography.Title>
+    },
+    {
+      title: "",
+      render: () => {
+        return (
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-between",
+              color: "#A5A8B1",
+              fontWeight: "500",
             }}
           >
-            <Search size="445px" placeholder="Tìm bằng số vé" />
-            <div style={{ marginTop: "-4px" }}>
-              <Button margin="0 10px" width="128px">
-                <span
-                  style={{
-                    display: "inline-block",
-                    transform: "translate(-6px, 4px)",
-                  }}
-                >
-                  <FilterIcon />
-                </span>
-                Lọc vé
-              </Button>
-              <Button width="180px">Xuất file (.csv)</Button>
-            </div>
+            Chưa đối soát
           </div>
-          <Table
-            bordered={false}
-            style={{ marginTop: "30px", alignItems: "center" }}
-            dataSource={dataTicketPage}
-            columns={columns}
-            pagination={{
-              pageSize: 12,
-              style: {
-                marginTop: "48px",
-                display: "flex",
-                justifyContent: "center",
-              },
-              size: "small",
-              prevIcon: () => <span style={{ color: "#A5A8B1" }}> &#9664;</span>,
-              nextIcon: <span style={{ color: "#A5A8B1" }}> &#9654;</span>,
-            }}
-          />
-        </Layout.Content>
-        <Layout.Content
+        );
+      },
+    },
+  ];
+
+  return (
+    <div style={{ display: "flex" }}>
+      <Layout.Content
+        style={{
+          borderRadius: "24px",
+          maxWidth: "1097px",
+          minHeight: "963px",
+          backgroundColor: "white",
+          padding: "28px",
+          marginRight: "32px",
+        }}
+      >
+        <Typography.Title
+          style={{ marginBottom: "46px", fontSize: "36px", fontWeight: "700" }}
+        >
+          Đối soát vé
+        </Typography.Title>
+        <div
           style={{
-            borderRadius: "24px",
-            minWidth: "446px",
-            minHeight: "963px",
-            backgroundColor: "white",
-            padding: "34px 24.5px",
+            display: "flex",
+            justifyContent: "space-between",
           }}
         >
-          <Typography.Title
-            style={{
-              marginTop: "-12px",
-              marginBottom: "46px",
-              fontSize: "24px",
-              fontWeight: "700",
-            }}
-          >
-            Lọc vé
-          </Typography.Title>
-        </Layout.Content>
-      </div>
-    );
-  };
+          <Search size="445px" placeholder="Tìm bằng số vé" />
+          <div style={{ marginTop: "-4px" }}>
+            <Button margin="0 10px" width="128px">
+              <span
+                style={{
+                  display: "inline-block",
+                  transform: "translate(-6px, 4px)",
+                }}
+              >
+                <FilterIcon />
+              </span>
+              Lọc vé
+            </Button>
+            <Button width="180px">Xuất file (.csv)</Button>
+          </div>
+        </div>
+        <Table
+          bordered={false}
+          style={{ marginTop: "30px", alignItems: "center" }}
+          dataSource={dataTicketPage}
+          columns={columns}
+          pagination={{
+            pageSize: 12,
+            style: {
+              marginTop: "48px",
+              display: "flex",
+              justifyContent: "center",
+            },
+            size: "small",
+            prevIcon: () => <span style={{ color: "#A5A8B1" }}> &#9664;</span>,
+            nextIcon: <span style={{ color: "#A5A8B1" }}> &#9654;</span>,
+          }}
+        />
+      </Layout.Content>
+      <Layout.Content
+        style={{
+          borderRadius: "24px",
+          minWidth: "446px",
+          minHeight: "963px",
+          backgroundColor: "white",
+          padding: "34px 24.5px",
+        }}
+      >
+        <Typography.Title
+          style={{
+            marginTop: "-12px",
+            marginBottom: "32px",
+            fontSize: "24px",
+            fontWeight: "700",
+          }}
+        >
+          Lọc vé
+        </Typography.Title>
+        <Row>
+          <Col span={11} style={{ fontSize: "16px" }}>
+            Tình trạng đối soát
+          </Col>
+          <Col span={11}>
+            <Radio.Group onChange={onChange} value={value}>
+              <Radio value={1} style={{ display: "block" }}>
+                Tất cả
+              </Radio>
+              <Radio value={2} style={{ display: "block" }}>
+                Đã đối soát
+              </Radio>
+              <Radio value={3} style={{ display: "block" }}>
+                Chưa đối soát
+              </Radio>
+            </Radio.Group>
+          </Col>
+        </Row>
+        <Row style={{ marginTop: "40px" }}>
+          <Col span={11} style={{ fontSize: "16px" }}>
+            Loại vé
+          </Col>
+          <Col span={11}>Vé cổng</Col>
+        </Row>
+        <Row style={{ marginTop: "24px" }}>
+          <Col span={11} style={{ fontSize: "16px" }}>
+            Từ ngày
+          </Col>
+          <Col span={11}>
+            <DatePicker
+              style={{ height: "40px", marginTop: "-6px", width: "100%" }}
+              defaultValue={moment("2021/05/01", "DD/MM/YYYY")}
+              format={"DD/MM/YYYY"}
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginTop: "30px" }}>
+          <Col span={11} style={{ fontSize: "16px" }}>
+            Đến ngày
+          </Col>
+          <Col span={11}>
+            <DatePicker
+              style={{ height: "40px", marginTop: "-6px", width: "100%" }}
+              placeholder="dd/mm/yy"
+              format={"DD/MM/YYYY"}
+            />
+          </Col>
+        </Row>
+        <Row style={{ margin: "32px auto" }}>
+          <Button margin="0 auto">Lọc</Button>
+        </Row>
+      </Layout.Content>
+    </div>
+  );
+};
+
+export default Doisoatve;
   
-  export default Doisoatve;
